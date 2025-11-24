7aee76b0cd56024f0edc54f854f82851c5375db7
Anushka Dongaonkar <d13anushka@gmail.com>
2025-11-24 00:22:27 -0500

    removed sql statements for activity_class_info

--- create_tables.sql
+++ create_tables.sql
@@ -1,24 +1,22 @@
--- select database
-USE starr_query;
-SHOW tables;
+
 
 -- drop table statements, "child" Associations table first
 DROP TABLE IF EXISTS Associations;
 DROP TABLE IF EXISTS Genes;
 DROP TABLE IF EXISTS Enhancers;
 
+
 -- create tables
 CREATE TABLE Enhancers (
 eid INTEGER NOT NULL AUTO_INCREMENT,
 name VARCHAR(30),
 chromosome VARCHAR(2),
-exp_condition ENUM('Control','20E','IMD'),
 start INTEGER,
 end INTEGER,
 en_length INTEGER,
+exp_condition ENUM('Control','20E','IMD'),
 tf_counts VARCHAR(150),
 tbs INTEGER,
-accessibility VARCHAR(50),
 PRIMARY KEY (eid),
 INDEX location (chromosome, start, end)
 ) ENGINE=innodb;
@@ -29,10 +27,10 @@ geneid VARCHAR(20),
 chromosome ENUM('3R','3L','2R','2L','X','Y','4'),
 start INTEGER,
 end INTEGER,
-gene_length INTEGER,
 symbol VARCHAR(20),
 immune_process VARCHAR(50),
 time_cluster ENUM('early_C2','mid_C3','late_C1','late_C4'),
+gene_length INTEGER,
 tpm_ctrl DOUBLE,
 tpm_20e DOUBLE,
 tpm_imd DOUBLE,
@@ -43,21 +41,16 @@ INDEX location (chromosome, start, end)
 CREATE TABLE Associations (
 eid INTEGER NOT NULL,
 gid INTEGER NOT NULL,
-imd_vs_ctrl DOUBLE,
 imd_vs_20e DOUBLE,
 20e_vs_ctrl DOUBLE,
+imd_vs_ctrl DOUBLE,
 exp_condition ENUM('Control', '20E', 'IMD'),
 activity DOUBLE,
-activity_class VARCHAR(30),
-accessibility VARCHAR(50),
-dist_to_enh INTEGER,
-time_cluster_broad VARCHAR(25),
-immune_process_broad VARCHAR(25),
+accessibility VARCHAR(30),
 PRIMARY KEY (eid, gid, exp_condition),
 FOREIGN KEY (eid) REFERENCES Enhancers (eid) ON UPDATE CASCADE ON DELETE CASCADE,
 FOREIGN KEY (gid) REFERENCES Genes (gid) ON UPDATE CASCADE ON DELETE CASCADE
 ) engine=innodb;
-
 
 -- loading in data
 -- note that empty fields are designated by NULL
@@ -67,7 +60,7 @@ INTO TABLE Enhancers
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES
 (name, chromosome, start, end, en_length, exp_condition,
- @tf_counts, @tbs, accessibility)
+ @tf_counts, @tbs)
 SET
   tf_counts     = NULLIF(@tf_counts, ''),
   tbs           = NULLIF(@tbs, '');
@@ -80,7 +73,6 @@ IGNORE 1 LINES
 SET immune_process = NULLIF(@immune_process, ''),
   time_cluster = NULLIF(@time_cluster, '');
 
-
 -- for Associations table, need to create a temp table first
 -- csv file has enhancer name and gene id so need to join tables
 
@@ -89,28 +81,23 @@ DROP TABLE IF EXISTS TempAssociations;
 CREATE TEMPORARY TABLE TempAssociations (
   enhancer_name VARCHAR(30),
   geneid VARCHAR(20),
-  imd_vs_ctrl DOUBLE,
   imd_vs_20e DOUBLE,
   20e_vs_ctrl DOUBLE,
-  exp_condition ENUM('Control', '20E', 'IMD'),
+  imd_vs_ctrl DOUBLE,
+  exp_condition ENUM('Control', '20E', 'HKSM'),
   activity DOUBLE,
-  accessibility VARCHAR(50),
-  dist_to_enh INTEGER,
-  time_cluster_broad VARCHAR(25),
-  immune_process_broad VARCHAR(25),
-  activity_class VARCHAR(30)
+  accessibility VARCHAR(30)
 ) ENGINE=innodb;
 
 LOAD DATA LOCAL INFILE '/Users/anushka/BU/starr_query/processed_data/associations.csv'
 INTO TABLE TempAssociations
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES
-(enhancer_name, geneid, @imd_vs_20e, @20e_vs_ctrl, @imd_vs_ctrl, exp_condition, activity, accessibility, @dist_to_enh, time_cluster_broad, immune_process_broad, activity_class)
+(enhancer_name, geneid, @imd_vs_20e, @20e_vs_ctrl, @imd_vs_ctrl, exp_condition, activity, accessibility)
 SET
   imd_vs_ctrl = NULLIF(@imd_vs_ctrl, ''),
   imd_vs_20e = NULLIF(@imd_vs_20e, ''),
-  20e_vs_ctrl = NULLIF(@20e_vs_ctrl, ''),
-  dist_to_enh = NULLIF(@dist_to_enh, '');
+  20e_vs_ctrl = NULLIF(@20e_vs_ctrl, '');
 
 -- create indexes to speed up joins
 CREATE INDEX idx_enhancer_name ON Enhancers(name);
@@ -128,11 +115,12 @@ JOIN Genes g ON t.geneid = g.geneid
 LIMIT 100;
 
 -- insert data from temp table to Associations table
-INSERT INTO Associations (eid, gid, imd_vs_ctrl, imd_vs_20e, 20e_vs_ctrl, exp_condition, activity, activity_class, accessibility, dist_to_enh, time_cluster_broad, immune_process_broad)
-SELECT e.eid, g.gid, t.imd_vs_ctrl, t.imd_vs_20e, t.`20e_vs_ctrl`, t.exp_condition, t.activity, t.activity_class, t.accessibility, t.dist_to_enh, t.time_cluster_broad, t.immune_process_broad
+INSERT INTO Associations (eid, gid, imd_vs_ctrl, imd_vs_20e, 20e_vs_ctrl, exp_condition, activity, accessibility)
+SELECT e.eid, g.gid, t.imd_vs_ctrl, t.imd_vs_20e, t.`20e_vs_ctrl`, t.exp_condition, t.activity, t.accessibility
 FROM TempAssociations t
 JOIN Enhancers e ON t.enhancer_name = e.name
 JOIN Genes g ON t.geneid = g.geneid;
+
 
 -- verify data loaded successfully
 SELECT COUNT(*) as num_genes FROM Genes;
