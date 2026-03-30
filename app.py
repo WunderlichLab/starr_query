85c3b628a2672fa7f2c93da6b8569998f84acba9
Anushka Dongaonkar <d13anushka@gmail.com>
2026-03-30 18:04:01 -0400

    minor changes for render

--- app.py
+++ app.py
@@ -571,8 +571,10 @@ def autocomplete_gene():
         print(f"Error in autocomplete: {e}")
         return jsonify([])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
