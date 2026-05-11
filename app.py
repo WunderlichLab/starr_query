721cd5a8cdac9e44ce34c1646372c7d92920ed61
Anushka Dongaonkar <d13anushka@gmail.com>
2026-05-10 20:53:50 -0400

    remove ssl line

--- app.py
+++ app.py
@@ -19,9 +19,7 @@ def connect_db():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
         database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306)),
-        ssl=True,
-        ssl_verify_cert=False
+        port=int(os.getenv("DB_PORT", 3306))
     )
 
 ## Get filter options:
